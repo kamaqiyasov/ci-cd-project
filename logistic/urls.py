79a17d13ffc413af0650028f5872738167e5e2cd
Kamil <kamil.qiyasov@mail.ru>
2026-03-20 14:54:19 +0300

    Add say hello api

--- logistic/urls.py
+++ logistic/urls.py
@@ -1,9 +1,12 @@
 from rest_framework.routers import DefaultRouter
-
-from logistic.views import ProductViewSet, StockViewSet
+from django.urls import path
+from logistic.views import ProductViewSet, StockViewSet, hello_world
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('stocks', StockViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    *router.urls,
+    path('sayhello/', hello_world, name='sayhello')
+]
--- logistic/views.py
+++ logistic/views.py
@@ -3,6 +3,8 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 class ProductViewSet(ModelViewSet):
@@ -17,3 +19,7 @@ class StockViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['products']
     search_fields = ['products__title', 'products__description']
+
+@api_view()
+def hello_world(request):
+    return Response({"message": "Hello, world!"})
